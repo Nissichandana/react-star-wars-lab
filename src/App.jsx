003b0199011fe3885d004cf86a75e23c43ba31bb
NissiChandana <nissi.neerudu@gmail.com>
2023-11-21 13:12:52 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import StarshipCard from './components/StarShipCard';
+import "./App.css"
+import "./index.css"
+
 const App = () => {
   const [starships, setStarships] = useState([]);
   useEffect(() => {
@@ -14,14 +18,18 @@ const App = () => {
     fetchStarships();
   }, []);
   return (
-    <div>
-       <header style={{position:'fixed',top:'0',left:'0',right:'0', border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '8px' , backgroundColor:'gray',textAlign:'center'}}><h1>Star Wars Starships</h1></header>
+    <>
+   <div> < header style={{position:'fixed',top:'0',left:'0', border: '1px', padding: '5px', margin: '5px', borderRadius: '8px' ,right:'0', bottom:'5', backgroundColor:'gray',textAlign:'center'}}><h1>Star Wars Starships</h1></header></div>
+    <div style={{display:'flex',flexWrap:'wrap'}}>
+      
+       
       {starships.map((starship, index) => (
-        <div key={index} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+        <div key={index} style={{ display:'flex',justifyContent:"center",alignItems:"center", border: '1px solid #ccc', margin: '70px', padding: '10px',width:"200px",boardrSizing:"boarder-box", borderRadius: '8px',backgroundColor: 'blue' }}>
           <h3>{starship.name}</h3>
         </div>
       ))}
     </div>
+    </>
   );
 };
 export default App;
